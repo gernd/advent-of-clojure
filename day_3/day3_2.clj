8805fdcd1fa3dd25373238e98534e21e82b472cb
Gernot Pointner <gernot.pointner@googlemail.com>
2018-01-27 01:12:07 +0100

    Remove unused param

--- day_3/day3_2.clj
+++ day_3/day3_2.clj
@@ -3,8 +3,8 @@
 (count squares))
 
 (defn get-first-value-above-threshold-helper
-[threshold steps-total squares direction take current-sprint current-steps]
-do (println "steps-total " steps-total " squares " squares " current steps " current-steps " current-sprint " current-sprint " direction " direction " take " take "threshold" threshold)
+[threshold squares direction take current-sprint current-steps]
+do (println " squares " squares " current steps " current-steps " current-sprint " current-sprint " direction " direction " take " take "threshold" threshold)
   (let [position (last squares)
         next-pos 
           (if (= :right direction) {:x (inc (get position :x)) :y (get position :y)}
@@ -15,19 +15,19 @@ do (println "steps-total " steps-total " squares " squares " current steps " cur
       (if (> value-of-current-square threshold) 
         value-of-current-square
       (if (< current-steps current-sprint)
-          (recur threshold (inc steps-total) (conj squares next-pos) direction take current-sprint (inc current-steps))
+          (recur threshold (conj squares next-pos) direction take current-sprint (inc current-steps))
           (let [next-direction
                   (if (= :right direction) :up
                   (if (= :up direction) :left
                   (if (= :left direction) :down
                   (if (= :down direction) :right))))]
               (if (= take 1)
-                  (recur threshold (inc steps-total) (conj squares next-pos) next-direction (inc take) current-sprint 1)
+                  (recur threshold (conj squares next-pos) next-direction (inc take) current-sprint 1)
               (if (= take 2)
-                  (recur threshold (inc steps-total) (conj squares next-pos) next-direction 1 (inc current-sprint) 1))))))))
+                  (recur threshold (conj squares next-pos) next-direction 1 (inc current-sprint) 1))))))))
 
 (defn get-first-value-above-threshold 
 [threshold]
-(get-first-value-above-threshold-helper threshold 1 [{:x 0 :y 0}] :right 1 1 1))
+(get-first-value-above-threshold-helper threshold [{:x 0 :y 0}] :right 1 1 1))
 
 (get-first-value-above-threshold 10)
