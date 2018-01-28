ab03c85e0779890aede52978e4ce481ffdafb795
Gernot Pointner <gernot.pointner@googlemail.com>
2018-01-28 01:40:16 +0100

    Add solution for day 3 second part

--- day_3/day3_2.clj
+++ day_3/day3_2.clj
@@ -1,6 +1,11 @@
-(defn get-value-of-square
-[squares]
-(count squares))
+(defn compute-value-of-square
+[pos squares]
+(let [x (get pos :x)
+      y (get pos :y)
+      neighbour-squares (filter #(and (<= (Math/abs(- (get % :x) x)) 1) (<= (Math/abs(- (get % :y) y)) 1) ) squares)
+      square-vals (map #(get % :val) neighbour-squares)
+      computed-square-val (reduce + square-vals)]
+  computed-square-val))
 
 (defn get-first-value-above-threshold-helper
 [threshold squares direction take current-sprint current-steps]
@@ -11,7 +16,7 @@ do (println " squares " squares " current steps " current-steps " current-sprint
           (if (= :up direction) {:x (get last-square :x) :y (inc (get last-square :y))}
           (if (= :down direction) {:x (get last-square :x) :y (dec (get last-square :y))}
           (if (= :left direction) {:x (dec (get last-square :x)) :y (get last-square :y)}))))
-        value-of-current-square (get-value-of-square squares)
+        value-of-current-square (compute-value-of-square current-pos squares)
         current-square {:x (get current-pos :x) :y (get current-pos :y) :val value-of-current-square}]
       (if (> value-of-current-square threshold) 
         value-of-current-square
@@ -31,4 +36,4 @@ do (println " squares " squares " current steps " current-steps " current-sprint
 [threshold]
 (get-first-value-above-threshold-helper threshold [{:x 0 :y 0 :val 1}] :right 1 1 1))
 
-(get-first-value-above-threshold 10)
+(get-first-value-above-threshold 325489)
