c49c858f7f74f6e495d05d3e2aeb1a217914c9b0
Gernot Pointner <gernot.pointner@googlemail.com>
2018-01-29 05:48:15 +0100

    Add solution for day 4 first part

--- day_4/day4_1.clj
+++ day_4/day4_1.clj
@@ -1,3 +1,5 @@
+(use 'clojure.java.io)
+
 (defn is-pw-valid-help
 [seen-pws remaining-pws]
 (do (println "seen-pws " seen-pws " remaining-pws " remaining-pws)
@@ -11,4 +13,16 @@
 [pws]
 (is-pw-valid-help [] pws))
 
-(is-pw-valid ["aa", "AA", "bb", "cc" "uu", "aa"])
+(defn get-lines
+      [fname]
+      (with-open [r (reader fname)]
+                (doall (line-seq r))))
+
+(defn load-phrases 
+[]
+(let [lines (get-lines "day_4/passphrases.txt")]
+  (map #(clojure.string/split % #" ") lines)))
+
+(let [phrases (load-phrases)
+      valid-phrases (map #(if (is-pw-valid %) 1 0) phrases)]
+    (reduce + 0 valid-phrases))
