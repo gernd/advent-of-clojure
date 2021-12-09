b2c916d123f0e4c11934c1dde643c2955398e2e9
Gernot Pointner <gernot.pointner@googlemail.com>
2021-12-09 20:47:02 +0100

    Solve day 1 first part

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/tools.trace "0.7.11"]
+                 ]
   :main ^:skip-aot clojureadv.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- src/clojureadv/2021/day1.clj
+++ src/clojureadv/2021/day1.clj
@@ -1,1 +1,34 @@
-(ns clojureadv.2021.day1)
+(ns clojureadv.2021.day1
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]
+            [clojure.tools.trace :refer :all]
+            ))
+
+(def test-data [213 123 123 123 12 20 19 18 100 200 300 400])
+
+(defn read-file-to-int-arr! [filename]
+  (->> filename
+       io/resource
+       slurp
+       str/split-lines
+       (map #(Integer/parseInt %))))
+
+(deftrace compute-sonar [history current]
+          (let [prev (:prev history)
+                result (:result history)]
+            (cond
+              (nil? prev) (assoc history :prev current :result (conj result :nil))
+              (< prev current) (assoc history :prev current :result (conj result :increased))
+              :else (assoc history :prev current :result (conj result :decreased))
+              )))
+
+(deftrace solve [data]
+          (->> data
+               (reduce compute-sonar {:prev nil :result (vector)})
+               (:result)
+               (filter #(= % :increased))
+               count))
+
+
+
+
