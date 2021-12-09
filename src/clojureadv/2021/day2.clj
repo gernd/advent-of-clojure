57852b3ca294f8a86fd4761f19659831a9e6d156
Gernot Pointner <gernot.pointner@googlemail.com>
2021-12-09 20:53:42 +0100

    Run cljfmt

--- src/clojureadv/2021/day2.clj
+++ src/clojureadv/2021/day2.clj
@@ -2,24 +2,21 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as str]
             [clojure.tools.trace :refer :all]
-            [clojureadv.2021.util :as :util]
-            ))
+            [clojureadv.2021.util :as :util]))
 
 (def test-data [213 123 123 123 12 20 19 18 100 200 300 400])
 
-
 (deftrace compute-sonar [history current]
-          (let [prev (:prev history)
-                result (:result history)]
-            (cond
-              (nil? prev) (assoc history :prev current :result (conj result :nil))
-              (< prev current) (assoc history :prev current :result (conj result :increased))
-              :else (assoc history :prev current :result (conj result :decreased))
-              )))
+  (let [prev (:prev history)
+        result (:result history)]
+    (cond
+      (nil? prev) (assoc history :prev current :result (conj result :nil))
+      (< prev current) (assoc history :prev current :result (conj result :increased))
+      :else (assoc history :prev current :result (conj result :decreased)))))
 
 (comment deftrace solve [data]
-          (->> data
-               (reduce compute-sonar {:prev nil :result (vector)})
-               (:result)
-               (filter #(= % :increased))
-               count))
+         (->> data
+              (reduce compute-sonar {:prev nil :result (vector)})
+              (:result)
+              (filter #(= % :increased))
+              count))
