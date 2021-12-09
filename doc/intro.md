fd7aaed82af345c6642bf6b12c51b312d43151bb
Gernot Pointner <gernot.pointner@googlemail.com>
2021-12-09 19:21:08 +0100

    Add new structure

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojureadv
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clojureadv/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojureadv.core-test
+  (:require [clojure.test :refer :all]
+            [clojureadv.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
